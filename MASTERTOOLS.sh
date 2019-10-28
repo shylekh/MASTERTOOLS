c99b8500181d05f3b1d5ab03bb2688d5ea132848
shylekh <56640724+shylekh@users.noreply.github.com>
2019-10-29 01:08:11 +0530

    MASTERTOOLS.sh
    This is a tool to install all the tools made by CYBER KALLAN

---
+++ MASTERTOOLS.sh
@@ -0,0 +1,167 @@
+#!/bash/bin
+sleep 1
+command clear
+toilet -f future "Coded by SHYLEKH" | lolcat
+sleep 2
+command clear
+toilet -f smblock "    MASTER TOOLS" | lolcat
+sleep 1
+echo
+echo
+echo
+echo "===========================================" | lolcat
+echo "Coded by SHYLEKH" | lolcat
+echo "Creadits for all this tools CYBER KALLAN" | lolcat
+echo "===========================================" | lolcat
+sleep 1
+echo
+echo
+echo
+
+green="\e[92m"
+printf "${green}"
+
+OPTIONS="CardersHub InstaSpamer HIDDENEYE InstaBruit BLACKEYE Unfollower AnonymousMail ClipboardMe ChoiceBot EasySpolit Inshackle EasyHack-CK Raavanan Userrecon Wordlist YoutubeBot Excuseme-Machine EXIT"
+select opt in $OPTIONS; do
+
+if [ "$opt" = "CardersHub" ]; then
+sleep 1
+figlet CardersHub | lolcat
+printf "${green}"
+echo
+command git clone https://github.com/cyberkallan/cardershub.git
+
+elif [ "$opt" = "InstaSpamer" ]; then
+sleep 1
+figlet InstaSpamer | lolcat
+printf "${green}"
+echo
+command git clone https://github.com/cyberkallan/Instapostspamer.git
+
+elif [ "$opt" = "HIDDENEYE" ]; then
+sleep 1
+figlet HIDDENEYE | lolcat
+printf "${green}"
+echo
+command git clone https://github.com/cyberkallan/hiddeneye-quick-installation-by-cyber-kallan.git
+
+elif [ "$opt" = "InstaBruit" ]; then
+sleep 1
+figlet InstaBruit | lolcat
+printf "${green}"
+echo
+command git clone https://github.com/cyberkallan/instashell-bruitforce.git
+
+elif [ "$opt" = "BLACKEYE" ]; then
+sleep 1
+figlet BLACKEYE | lolcat
+printf "${green}"
+echo
+command git clone https://github.com/cyberkallan/blackeye.git
+
+elif [ "$opt" = "Unfollower" ]; then
+sleep 1
+figlet Unfollower | lolcat
+printf "${green}"
+echo
+command git clone https://github.com/cyberkallan/unfollower.git
+
+elif [ "$opt" = "AnonymousMail" ]; then
+sleep 1
+figlet AnonymousMail | lolcat
+printf "${green}"
+echo
+command git clone https://github.com/cyberkallan/anonymous-mail.git
+
+elif [ "$opt" = "ClipboardMe" ]; then
+sleep 1
+figlet ClipboardMe | lolcat
+printf "${green}"
+echo
+command git clone https://github.com/cyberkallan/clipboardme.git
+
+elif [ "$opt" = "ChoiceBot" ]; then
+sleep 1
+figlet ChoiceBot | lolcat
+printf "${green}"
+echo
+command git clone https://github.com/cyberkallan/choicebot.git
+
+elif [ "$opt" = "EasySpolit" ]; then
+sleep 1
+figlet EasySpolit | lolcat
+printf "${green}"
+echo
+command git clone https://github.com/cyberkallan/easysploit.git
+
+elif [ "$opt" = "Inshackle" ]; then
+sleep 1
+figlet Inshackle | lolcat
+printf "${green}"
+echo
+command git clone https://github.com/cyberkallan/inshackle-bot.git
+
+elif [ "$opt" = "EasyHack-CK" ]; then
+sleep 1
+figlet EasyHack-CK | lolcat
+printf "${green}"
+echo
+command git clone https://github.com/cyberkallan/Easyhack-CK.git
+
+elif [ "$opt" = "Raavanan" ]; then
+sleep 1
+figlet Raavanan | lolcat
+printf "${green}"
+echo
+command git clone https://github.com/cyberkallan/raavanan.infog.git
+
+elif [ "$opt" = "Userrecon" ]; then
+sleep 1
+figlet Userrecon | lolcat
+printf "${green}"
+echo
+command git clone https://github.com/cyberkallan/userrecon.git
+
+elif [ "$opt" = "Wordlist" ]; then
+sleep 1
+figlet Wordlist | lolcat
+printf "${green}"
+echo
+command git clone https://github.com/cyberkallan/wordlistor.git
+
+elif [ "$opt" = "YoutubeBot" ]; then
+sleep 1
+figlet YoutubeBot | lolcat
+printf "${green}"
+echo
+command git clone https://github.com/cyberkallan/youtubebot.git
+
+elif [ "$opt" = "Excuseme-Machine" ]; then
+sleep 1
+figlet Excuseme-Machine | lolcat
+printf "${green}"
+echo
+command git clone https://github.com/cyberkallan/excuseme-machane.git
+
+elif [ "$opt" = "EXIT" ]; then
+printf "${green}"
+echo 😉
+echo THANKS FOR USING
+echo Coded by SHYLEKH
+echo Creadits for all the tools CYBER KALLAN
+echo DONOT USE THIS TOOL FOR ANY ILLEGEL ACTIVITIES
+sleep 3
+command clear
+toilet -f future "Coded by SHYLEKH" | lolcat
+sleep 1
+command clear
+command exit
+
+else
+printf "${green}"
+echo ☹
+echo INVALID INPUT
+command exit
+
+fi
+done
